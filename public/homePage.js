9bad1025207ad638be3dbc0dabe66d9b6dc6a040
petka61 <petka061@gmail.com>
2022-03-13 22:59:46 +0300

    exercise fixed and second part is done

---
+++ public/homePage.js
@@ -0,0 +1,102 @@
+"use strict";
+let logOut = new LogoutButton();
+logOut.action = function() {
+  ApiConnector.logout(request => {
+    if (request.success) {
+      location.reload();
+    }
+  });
+}
+
+ApiConnector.current(check => {
+  if (check.success) {
+    return ProfileWidget.showProfile(check.data);
+  } else {
+    console.error("Пользователь не авторизован");
+  }
+});
+
+let rateBoard = new RatesBoard();
+let refreshRatesBoard = function() {
+  ApiConnector.getStocks(request => {
+    if (request.success) {
+      rateBoard.clearTable();
+      rateBoard.fillTable(request.data);
+    }
+  })
+};
+
+refreshRatesBoard();
+setInterval(() => refreshRatesBoard(), 60000);
+
+let money = new MoneyManager();
+money.addMoneyCallback = function(data) {
+  ApiConnector.addMoney(data, request => {
+    if (request.success) {
+      ProfileWidget.showProfile(request.data);
+      money.setMessage(true, `Баланс пополнен на ${data.amount}`)
+    } else {
+      money.setMessage(false, `Пополнение баланса не состоялось`)
+    }
+  })
+};
+
+money.conversionMoneyCallback = function(data) {
+  ApiConnector.convertMoney(data, request => {
+    if (request.success) {
+      ProfileWidget.showProfile(request.data);
+      money.setMessage(true, `Конвертация прошла успешно`)
+    } else {
+      money.setMessage(false, `Конвертации не произошло`)
+    }
+  })
+};
+
+money.sendMoneyCallback = function(data) {
+  ApiConnector.transferMoney(data, request => {
+    if (request.success) {
+      ProfileWidget.showProfile(request.data);
+      money.setMessage(true, `Перевод прошел успешно`)
+    } else {
+      money.setMessage(false, `Перевод не состоялся`)
+    }
+  })
+};
+
+let favoritWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(request => {
+  if (request.success) {
+    favoritWidget.clearTable();
+    favoritWidget.fillTable(request.data);
+    money.updateUsersList(request.data);
+  }
+});
+
+favoritWidget.addUserCallback = function(data) {
+  ApiConnector.addUserToFavorites(data, request => {
+    if (request.success) {
+      favoritWidget.clearTable();
+      favoritWidget.fillTable(request.data);
+      money.updateUsersList(request.data);
+      money.setMessage(true, `Добавление прошло успешно`)
+    } else {
+      money.setMessage(false, `Добавление не состоялось. Указанный ID уже существует.`)
+    }
+  })
+};
+
+favoritWidget.removeUserCallback = function(data) {
+  ApiConnector.removeUserFromFavorites(data, request => {
+    if (request.success) {
+
+      for (let i in request.data) {
+        console.log(i)
+      }
+      ProfileWidget.showProfile(data);
+      money.setMessage(true, `Пользователь удален`)
+    } else {
+      money.setMessage(false, `Удаление не состоялось`)
+    }
+  })
+};
--- public/loginPage.js
+++ public/loginPage.js
@@ -2,23 +2,22 @@
 let loginObj = new UserForm();
 
 loginObj.loginFormCallback = function(data) {
-  ApiConnector.login({data}, response =>{
+  ApiConnector.login(data, response =>{
     console.log(response);
   if (response.success) {
     location.reload();
   } else {
-    console.error(`Пользователь c логином ${data.login} и указанным паролем не найден`)
+    loginObj.setLoginErrorMessage(`Пользователь c логином ${data.login} и указанным паролем не найден`);
   }
 });
 };
 
 loginObj.registerFormCallback = function(data) {
-  ApiConnector.register({data}, response => {
-    console.log(response);
+  ApiConnector.register(data, response => {
     if (response.success) {
       location.reload();
     } else {
-      console.error(`Пользователь не зарегистрирован`)
+      loginObj.setRegisterErrorMessage(`Логин ${data.login} уже существует.`);
     }
   })
 }
